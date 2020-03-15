6648329ad3f00a94c5c539845b94fb61705b6aa3
flowalex <flowalex@vivaldi.net>
2020-03-14 20:02:14 -0500

    Chapter 15 Updating app structre

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,6 @@ import os
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -19,6 +17,16 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
 
 if not app.debug:
     if not os.path.exists('logs'):
@@ -33,4 +41,4 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
     
-from app import routes, models, errors
+from app import models
